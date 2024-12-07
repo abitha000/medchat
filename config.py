b6d80ca2bd3cd2516de4e066b26c81340a4003c7
abitha000 <abithak000@yahoo.com>
2024-12-07 23:43:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import re
-import os
+from os import getenv
 
 
-BOT_TOKEN = "7548088682:AAFL08f6rTFBErJhbDK3uMMC7n_ZJDe3_QM"
+BOT_TOKEN = getenv("BOT_TOKEN", "7548088682:AAFL08f6rTFBErJhbDK3uMMC7n_ZJDe3_QM")
